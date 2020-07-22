aa6f97510171ad85b783dabe9e9d9cd51c719537
vamsi <vamsik5555@gmail.com>
2020-07-22 15:37:03 +0530

    new changes

--- src/test/java/mavenproject/Test1.java
+++ src/test/java/mavenproject/Test1.java
@@ -1,5 +1,33 @@
 package mavenproject;
 
-public class Test1 {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+	public static WebDriver driver;
+
+	@BeforeMethod
+	public void setup() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void login() {
+		driver.get("https://google.com");
+		System.out.println("Redirected to Google: Test is pass");
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+		System.out.println("Closing browser");
+
+	}
 }
